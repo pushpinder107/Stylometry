d4e3e3f37ebc4e5972f7ab7bf5090e1ef5d21f3c
skarthikeya <karthikeya.surabhi@gmail.com>
2016-08-08 19:01:02 +0530

    Updated bigram generator file

--- characterCount.java
+++ characterCount.java
@@ -7,15 +7,30 @@ public class characterCount {
 	public static HashMap<String,String> generateBigrams(ArrayList<String> words){
 		
 		HashMap<String,String> bigrams=new HashMap<String,String>();
-		for(int i=0;i<words.size();i++){
-			if(words.get(i+2)!=""){
-				bigrams.put(words.get(i)+words.get(i+1), words.get(i+2));
+		for(int i=0;i<words.size()-2;i++){
+			if(words.get(i)!="\n" && !words.get(i).contains(".") && !words.get(i+1).contains(".") ){
+				
+			String valueWord=words.get(i+2);
+			if(!valueWord.contains(".")){
+				words.get(i+2).replace(".", "");
+				bigrams.put(words.get(i)+ " " + words.get(i+1), words.get(i+2));
 								
 			}
+			else
+			{
+				//System.out.println("Valueword is"+ valueWord);
+				valueWord.replace(".", " ");
+				//words.get(i+2).replace(".", "");
+				bigrams.put(words.get(i)+words.get(i+1), words.get(i+2));
+				
+				
+				}
+			}
+		
+		
+		
 		}
-		
 		return bigrams;
-		
 	}
 
 }
